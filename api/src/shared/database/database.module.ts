90ac3a28c74cbd3ba33d9e7f70b08cdaf2150794
viniciusrbr <viniciusgoes159@gmail.com>
2025-10-21 21:02:46 -0300

    feat: bank-account module

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -2,10 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositores/users.repositories';
 import { CategoriesRepository } from './repositores/categories.repositories';
+import { BankAccountsRepository } from './repositores/bank-accounts';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, CategoriesRepository],
-  exports: [UsersRepository, CategoriesRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
+  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
 })
 export class DatabaseModule {}
