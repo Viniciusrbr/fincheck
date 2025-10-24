99a768713fd32c59f53d031388b5226401eb0c7e
viniciusrbr <viniciusgoes159@gmail.com>
2025-10-24 17:47:36 -0300

    feat: web setup with auth layout

---
+++ front-end/src/Router/index.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import { AuthLayout } from '../view/layouts/AuthLayout'
+import { Dashboard } from '../view/pages/Dashboard'
+import { Login } from '../view/pages/Login'
+import { Register } from '../view/pages/Register'
+import { AuthGuard } from './AuthGuard'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthGuard isPrivate={true} />}>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Route>
+
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
