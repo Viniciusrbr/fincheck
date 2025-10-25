d6bbf2476b35d60a0b56c6022928f03c6c558db3
viniciusrbr <viniciusgoes159@gmail.com>
2025-10-25 17:40:12 -0300

    feat: login and register ui

---
+++ front-end/src/view/components/Button.tsx
@@ -0,0 +1,15 @@
+import { type ComponentProps } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed
+      disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all"
+    >
+      {children}
+    </button>
+  )
+}
--- front-end/src/view/pages/Login/index.tsx
+++ front-end/src/view/pages/Login/index.tsx
@@ -1,3 +1,36 @@
+import { Link } from 'react-router'
+
+import { Button } from '../../components/Button'
+import { Input } from '../../components/input'
+
 export function Login() {
-  return <h1>Login Page</h1>
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Novo por aqui?
+          </span>
+          <Link
+            to="/register"
+            className="tracking-[-1px] text-teal-900 font-medium"
+          >
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Senha" />
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </>
+  )
 }
