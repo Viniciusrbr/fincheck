fc34ce7ff763f9ccf4c0328e475ef6a8c78be44d
viniciusrbr <viniciusgoes159@gmail.com>
2025-11-03 19:19:12 -0300

    feat: dashboard transactions and accounts ui

---
+++ front-end/src/vite-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module 'swiper/css' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module 'swiper/css/*' {
+  const content: Record<string, string>
+  export default content
+}
